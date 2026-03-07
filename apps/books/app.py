d21696897829471db09fb503ec091a3bc99c9fc7
ManmathGantayat <54932740+ManmathGantayat@users.noreply.github.com>
2026-03-07 14:48:20 +0530

    Update app.py

--- apps/books/app.py
+++ apps/books/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", title="TEST BOOKS Application v2", category="Books")
+    return render_template("index.html", title="TEST BOOKS Application v3", category="Books")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
